91a32f95abe1930dfb2eb3638384e35c0eb30773
kuqmua <serezhashmakov@outlook.com>
2022-04-26 22:11:56 +0300

    reusage trait handle

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ pub fn derive_provider_kind_from_config(input: TokenStream) -> TokenStream {
     let data: Data = ast.data;
     let trait_name: Ident;
     let function_vec_idents: Vec<(Ident, ReturnType)>;
-    let trait_handle = r#"pub trait ProviderKindFromConfigTrait {
+    let trait_handle = quote! {
+        pub trait ProviderKindFromConfigTrait {
         #[deny(
             clippy::indexing_slicing,
             clippy::unwrap_used,
@@ -173,7 +174,7 @@ pub fn derive_provider_kind_from_config(input: TokenStream) -> TokenStream {
         )]
         fn links_limit(&self) -> i64;
     }
-    "#.to_string();
+};
     // let trait_ident = syn::Ident::new(
     //     &trait_handle,
     //     ident.span(),
@@ -203,7 +204,7 @@ pub fn derive_provider_kind_from_config(input: TokenStream) -> TokenStream {
     //             .collect();
     //     }
     // }
-    let token_stream: proc_macro::TokenStream = trait_handle
+    let token_stream: proc_macro::TokenStream = trait_handle.to_string()
                 .parse()
                 .expect("cannot parse file into proc_macro::TokenStream");
             let trait_ast: ItemTrait = syn::parse(token_stream)
@@ -277,160 +278,7 @@ pub fn derive_provider_kind_from_config(input: TokenStream) -> TokenStream {
         });
     }
     let generated = quote! {
-        pub trait ProviderKindFromConfigTrait {
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_mongo_initialization_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_mongo_write_error_logs_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_mongo_cleaning_warning_logs_db_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_mongo_cleaning_warning_logs_db_collections_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_mongo_link_parts_randomize_order_enabled(&self) -> bool;
-        
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_postgres_initialization_enabled(&self) -> bool;
-        
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_write_error_logs_in_local_folder_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_cleaning_warning_logs_directory_enabled(&self) -> bool;
-        
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn check_link(&self) -> &'static str;
-        
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_dbs_initialization_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_warning_high_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_warning_low_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_success_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_partial_success_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_error_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_time_measurement_prints_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_info_prints_enabled(&self) -> bool;
-        
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn is_links_limit_enabled(&self) -> bool;
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            fn links_limit(&self) -> i64;
-        }
+        #trait_handle
         impl #trait_name for #ident {
             #(#function_quote_vec_ident)*
         }
